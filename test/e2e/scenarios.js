9e7c166ae9ef38fbb98c54d3321191d9f237ae66
Ken Ho <ken.ho170381@gmail.com>
2015-09-17 15:28:14 +0700

    step 3

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -2,10 +2,25 @@
 
 /* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
 
-describe('my app', function() {
+describe('Phone App', function() {
+  describe('Phone list view', function() {
+    beforeEach(function () {
+      browser.get('index.html');
+    });
+    it('should filter the phone list as a user types into the search box', function() {
+      var phoneList = element.all(by.repeater('phone in phones'));
+      var query = element(by.model('query'));
 
-  beforeEach(function() {
-    browser.get('index.html');
+      expect(phoneList.count()).toBe(3);
+
+      query.sendKeys('nexus');
+      expect(phoneList.count()).toBe(1);
+
+
+      query.clear();
+      query.sendKeys('motorola');
+      expect(phoneList.count()).toBe(2);
+
+    });
   });
-
 });
